8d8b406af50cdc4158c331396f20a552417b7df4
MBorse29 <manojborse29@gmail.com>
2019-05-06 10:44:35 +0530

    Init
    First commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import WeatherInfo from "./WeatherInfo.jsx";
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button>Get the weather Info</button>
+      <WeatherInfo info={{}} />
     </div>
   );
 }
